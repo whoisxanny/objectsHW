2b120ee25f0870c1da1dda3473ade064df256112
whoisxanny <tuffx4nny@gmail.com>
2023-10-21 14:00:31 +0500

    hwRemastered

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Author oneA = new Author("Allan","Timurovich");
-        Book one = new Book("Savage",2022,oneA);
-        System.out.println(one.getName());
-        System.out.println(one.getYear());
-
-        Author twoA = new Author("Boris","Akunin");
-        Book two = new Book("Ves' Mir Teatr",2010,twoA);
-        two.setYear(2011);
-        System.out.println("two.getYear() = " + two.getYear());
-
 
     }
 }
--- src/Run.java
+++ src/Run.java
@@ -1,4 +1,15 @@
 public class Run {
+    public static void main(String[] args){
+        Author oneA = new Author("Allan","Timurovich");
+        Book one = new Book("Savage",2022,oneA);
+        System.out.println(one.getName() + " " + one.getYear() + " " + one.getAuthor().getName() + " " + one.getAuthor().getSurName());
 
+
+        Author twoA = new Author("Boris","Akunin");
+        Book two = new Book("Ves' Mir Teatr",2010,twoA);
+        two.setYear(2011);
+        System.out.println("two.getYear() = " + two.getYear());
+        System.out.println(two.getName() + " " + two.getYear() + " " + two.getAuthor().getName() +  " " + two.getAuthor().getSurName());
+    }
 
 }
